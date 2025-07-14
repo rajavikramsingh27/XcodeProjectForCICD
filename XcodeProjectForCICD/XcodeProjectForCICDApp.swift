cdeecd10d80e2f3dd6faf3552501246b432e978a
Vikram Singh <rajavikramsingh27@gmail.com>
2025-07-14 08:15:12 +0530

    First commit

---
+++ XcodeProjectForCICD/XcodeProjectForCICDApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeProjectForCICDApp.swift
+//  XcodeProjectForCICD
+//
+//  Created by Vikram on 14/07/25.
+//
+
+import SwiftUI
+
+@main
+struct XcodeProjectForCICDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ XcodeProjectForCICDUITests/XcodeProjectForCICDUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  XcodeProjectForCICDUITestsLaunchTests.swift
+//  XcodeProjectForCICDUITests
+//
+//  Created by Vikram on 14/07/25.
+//
+
+import XCTest
+
+final class XcodeProjectForCICDUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
